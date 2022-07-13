724e062ae43cc8ffacf1ed81fbaf17290291ac60
Tyler Nguyen <tynguyen06@gmail.com>
2022-07-13 11:06:27 -0700

    add json body parser

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('./User/User');
 
 const app = express();
+app.use(express.json());
 
 app.post('/api/1.0/users', (req, res) => {
   User.create(req.body).then(() => {
