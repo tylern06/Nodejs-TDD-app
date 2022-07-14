cad6f549f99d78ba30d9ebbc39944adc309fe034
Tyler Nguyen <tynguyen06@gmail.com>
2022-07-14 14:43:27 -0700

    add test and dev db config

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,13 @@
 const Sequelize = require('sequelize');
-
+const config = require('config');
+// get database config
+const _config = config.get('database');
+// console.log('env:', process.env, _config);
 // initailize db instance with connection string
-const sequelize = new Sequelize('hoaxify', 'db-user', 'db-password', {
-  dialect: 'sqlite',
-  storage: './database.sqlite',
-  logging: false,
+const sequelize = new Sequelize(_config.database, _config.username, _config.password, {
+  dialect: _config.dialect,
+  storage: _config.storage,
+  logging: _config.dialect.logging,
 });
 
 module.exports = sequelize;
